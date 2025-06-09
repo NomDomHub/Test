64580539e6b7c57e41d98804a1a6ccb01cf92ab7
NomDomHub <duyanhkhanh0003@gmail.com>
2025-06-09 21:41:52 +0700

    Update WedbookNuke.lua

--- WedbookNuke.lua
+++ WedbookNuke.lua
@@ -1,14 +1,31 @@
+getgenv().Wedbook = {
+    ["Wedbook"] = {
+        ["Url"] = "",
+        ["Delay"] = 1,     
+        ["Repeat"] = "",   
+        ["Content"] = {
+            ["Content"] = "",
+            ["Ping Everyone"] = false,
+            ["Ping Here"] = false,
+            ["Ping Id Role"] = "",
+            ["Ping Id User"] = "",
+        }
+    }
+}
+
 task.spawn(function()
     local HttpService = game:GetService("HttpService")
-
     local config = getgenv().Wedbook and getgenv().Wedbook.Wedbook
     if not config or not config.Url then return end
 
     local url = config.Url
-    local delayTime = tonumber(config.Delay) or 0
-    local contentConfig = config.Content or {}
+    local delayTime = tonumber(config.Delay) or 1
+    local repeatCount = tonumber(config.Repeat)
+    if not repeatCount or repeatCount <= 0 then
+        repeatCount = math.huge -- vô hạn
+    end
 
-    task.wait(delayTime)
+    local contentConfig = config.Content or {}
 
     local messageParts = {}
 
@@ -20,23 +37,19 @@ task.spawn(function()
         table.insert(messageParts, "@here")
     end
 
-    if contentConfig["Ping Id Role"] and contentConfig["RoleId"] then
-        table.insert(messageParts, "<@&" .. tostring(contentConfig["RoleId"]) .. ">")
+    if contentConfig["Ping Id Role"] and contentConfig["Ping Id Role"] ~= "" then
+        table.insert(messageParts, "<@&" .. tostring(contentConfig["Ping Id Role"]) .. ">")
     end
 
-    if contentConfig["Ping Id User"] and contentConfig["UserId"] then
-        table.insert(messageParts, "<@" .. tostring(contentConfig["UserId"]) .. ">")
+    if contentConfig["Ping Id User"] and contentConfig["Ping Id User"] ~= "" then
+        table.insert(messageParts, "<@" .. tostring(contentConfig["Ping Id User"]) .. ">")
     end
 
-    if contentConfig.Content then
+    if contentConfig.Content and contentConfig.Content ~= "" then
         table.insert(messageParts, tostring(contentConfig.Content))
     end
 
     local finalMessage = table.concat(messageParts, " ")
-
-    local payload = {
-        content = finalMessage
-    }
 
     local request =
         (typeof(http_request) == "function" and http_request)
@@ -44,14 +57,17 @@ task.spawn(function()
         or (syn and syn.request)
         or (fluxus and fluxus.request)
 
-    if request then
-        pcall(function()
-            request{
-                Url = url,
-                Method = "POST",
-                Headers = { ["Content-Type"] = "application/json" },
-                Body = HttpService:JSONEncode(payload)
-            }
-        end)
+    for i = 1, repeatCount do
+        if request then
+            pcall(function()
+                request{
+                    Url = url,
+                    Method = "POST",
+                    Headers = { ["Content-Type"] = "application/json" },
+                    Body = HttpService:JSONEncode({ content = finalMessage })
+                }
+            end)
+        end
+        task.wait(delayTime)
     end
 end)
